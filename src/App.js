83c03a8999920112b006844d56ae223eab5c4449
codemonkey30 <=>
2021-11-30 12:21:50 +0530

    add device detech and env packages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import { BrowserView, MobileView } from "react-device-detect";
+
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
-  return <div className="container">test</div>;
+  return (
+    <>
+      <BrowserView>
+        <h1>This is rendered only in browser</h1>
+      </BrowserView>
+      <MobileView>
+        <h1>This is rendered only on mobile</h1>
+      </MobileView>
+    </>
+  );
 };
