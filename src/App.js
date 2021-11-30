f55d4e1f031c74d5cd418260a024bc65b6afd83c
codemonkey30 <=>
2021-11-30 13:52:39 +0530

    dotenv is removed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { BrowserView, MobileView } from "react-device-detect";
+import { auth } from "./firebase";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  console.log(auth);
   return (
     <>
       <BrowserView>
         <h1>This is rendered only in browser</h1>
       </BrowserView>
       <MobileView>
-        <h1>This is rendered only on mobile</h1>
+        <h1>Under Construction</h1>
       </MobileView>
     </>
   );
